e91d866781fedd42b17b0a29acb83b90062ca828
David Goffredo <david.goffredo@datadoghq.com>
2023-09-26 18:50:05 -0400

    instrumentation/httpd is working on RHEL 8.8

--- instrumentation/httpd/build.sh
+++ instrumentation/httpd/build.sh
@@ -9,4 +9,9 @@ set -e
 #     --compilation_mode=<fastbuild, dbg or opt> [-c] default: "fastbuild"
 #         Specify the mode the binary will be built in. Values: 'fastbuild', 'dbg', 'opt'.
 
-bazelisk build --verbose_failures -c opt :all
+bazelisk build --nostart_end_lib --verbose_failures --subcommands -c opt :all
+
+# Now redo the final linking step, but use "g++" as the linking driver instead
+# of "gcc". This allows the "-static-libstdc++" and "-static-libgcc" options to
+# work more easily.
+<bazel-out/k8-opt/bin/otel.so-2.params ./transform_params | tr '\n' '\0' | xargs -0 g++
--- instrumentation/httpd/setup-buildtools.sh
+++ instrumentation/httpd/setup-buildtools.sh
@@ -5,9 +5,8 @@ set -x
 set -e
 
 sudo yum group install -y "Development Tools"
-sudo yum install -y automake libtool curl libcurl-devel zlib-devel git openssl-devel wget # apr-devel
+sudo yum install -y automake libtool curl libcurl-devel zlib-devel git openssl-devel wget
 
-# BAZELISK_VERSION=v1.7.4
 BAZELISK_VERSION=v1.18.0
 
 wget -O /tmp/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/$BAZELISK_VERSION/bazelisk-linux-amd64
